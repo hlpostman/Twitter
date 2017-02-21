836b51ec86287f800a1b75b81904556fdc5f089d
Hannah Lily Postman <postman.hl@gmail.com>
2017-02-21 15:06:33 -0800

    Basic Tweet object set up.  Video 2 complete.  Everything working.  Just warnings for unused tasks mostly.

--- Twitter/Tweet.swift
+++ Twitter/Tweet.swift
@@ -9,5 +9,37 @@
 import UIKit
 
 class Tweet: NSObject {
+    var text: NSString?
+    var retweetCount: Int = 0
+    var likeCount: Int = 0
+    var timestamp: NSDate?
+    
+    init(dictionary: NSDictionary) {
+        
+        // Strings and Integers
+        text = dictionary["text"] as? NSString
+        retweetCount = (dictionary["retweet_count"] as? Int) ?? 0
+        likeCount = (dictionary["favourite_count"] as? Int) ?? 0
+        
+        // Timestamp
+        let timestampString = dictionary["created_at"] as? String
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE MM d HH:mm:ss Z y"
+        if let timestampString = timestampString {
+            timestamp = formatter.date(from: timestampString) as NSDate?
 
+        }
+        
+    }
+    
+    class func tweetsWithArray(dictionaries: [NSDictionary]) -> [Tweet] {
+        var tweets = [Tweet]()
+        for dictionary in dictionaries {
+            let tweet = Tweet(dictionary: dictionary)
+            tweets.append(tweet)
+        }
+        
+        return tweets
+    }
+    
 }
