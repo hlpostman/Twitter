b9ce9afdb67a5bda13a27e16de6d9aa9f321039e
Hannah Lily Postman <postman.hl@gmail.com>
2017-02-20 17:05:36 -0800

    Building, but not getting token.  At about minute 12 in video 1.

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -24,18 +24,18 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func onLoginButton(_ sender: AnyObject) {
-        let twitterClient = BDBOAuth1SessionManager(baseURL: NSURL(string: "https://api.twitter.com")!, consumerKey: "UOgFeSdrTPCdG5aCPS933gOso", consumerSecret: "EoeUPlkFlicEdf28GLC8M28apkgkIkKHQPnzsTtcyUJP4IPJh")
+        let twitterClient = BDBOAuth1SessionManager(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "UOgFeSdrTPCdG5aCPS933gOso", consumerSecret: "EoeUPlkFlicEdf28GLC8M28apkgkIkKHQPnzsTtcyUJP4IPJh")
         
-        twitterClient.deauthorize()
-        twitterClient.fetchRequestToken(with: "oauth/request_token", method: "GET", callbackURL: NSURL(string: "twitterhlp://oauth"), scope: nil, success: { (requestToken:
-            BDBOAuth1Credential!) -> Void in
+        twitterClient?.deauthorize()
+        twitterClient?.fetchRequestToken(withPath: "oauth/request_token", method: "GET", callbackURL: NSURL(string: "twitterhlp://oauth") as URL!, scope: nil, success: { (requestToken:
+            BDBOAuth1Credential?) in
             print("I got a token!")
             
-            let url = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token\(requestToken.token)")!
-            UIApplication.sharedApplication().openURL(url)
+            let url = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token\(requestToken!.token!)")!
+            UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
             
-        }) { (error: NSError!) -> Void in
-            print("error: \(error.localizedDescription)")
+        }) { (error: Error?) -> Void in
+            print("error: \(error?.localizedDescription)")
         }
     }
 
