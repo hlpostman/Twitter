169658dc1c6332a5c357b945b3369a3a64f4ffd6
Hannah Lily Postman <postman.hl@gmail.com>
2017-03-03 09:36:49 -0800

    Add all the outlets to xib

--- Twitter/DetailView.swift
+++ Twitter/DetailView.swift
@@ -11,8 +11,26 @@ import UIKit
 
 class DetailView: UIView {
 
-    @IBOutlet weak var nameLabel: UILabel!
+
     @IBOutlet weak var view: UIView!
+    @IBOutlet weak var mainParentView: UIView!
+    @IBOutlet weak var profilePicImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var handleLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+   
+    @IBOutlet weak var bigRetweetCountLabel: UILabel!
+    @IBOutlet weak var bigLikeCountLabel: UILabel!
+
+    @IBOutlet weak var timePostedLabel: UILabel!
+    
+    @IBOutlet weak var replyButton: UIButton!
+    @IBOutlet weak var replyCountLabel: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var smallRetweetCountLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var smallLikeCountLabel: UILabel!
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,6 +47,7 @@ class DetailView: UIView {
         Bundle.main.loadNibNamed("DetailView", owner: self, options: nil)
         view.frame = self.frame
         view.backgroundColor = UIColor.red
+        mainParentView.layer.cornerRadius = 5
         view.layoutIfNeeded()
         self.addSubview(view)
     }
