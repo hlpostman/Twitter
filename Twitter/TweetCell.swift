2babf0e9158f776187afd3fb4476663455b01045
Hannah Lily Postman <postman.hl@gmail.com>
2017-03-04 22:55:43 -0800

    Connect xib elements to TweetCell class

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -42,16 +42,6 @@ class TweetCell: UITableViewCell {
     
     weak var delegate: TweetTableViewCellDelegate?
     
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
